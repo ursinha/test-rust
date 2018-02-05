da33fab0dae9f55b880b910f8ec2b2d54b41fcbe
Ursula Junque (Ursinha) <ujunque@isilon.com>
2018-02-05 18:45:07 -0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+extern crate rand;
+
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    let mut attempts = 0;
+
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+            attempts += 1;
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You guessed right!");
+                break;
+            },
+            Ordering::Greater => println!("Too big"),
+        }
+    }
+    println!("Number of attempts: {}", attempts);
+}
